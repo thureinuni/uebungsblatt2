5dccef269a7f26fca190cc4ecff360a18fb7dc63
thureinuni <tthu1@smail.uni-koeln.de>
2025-10-31 16:55:44 +0100

    wirklich finaler push

--- src/h2/H2_main.java
+++ src/h2/H2_main.java
@@ -3,7 +3,7 @@ package h2;
 public class H2_main {
 	public static void main (String args[]) {
 		int i = 7 ;
-		int j = 5;
+		int j = 900;
 		int k = 400;
 		int min = 0 ; // Damit min initialisiert ist
 		int max = 0;  // Damit max inittialisiert ist
@@ -11,30 +11,30 @@ public class H2_main {
 		
 		// Um das Minimum aus i, j, k zu finden, dadurch das man mehrere Vergleiche hat && benutzt
 		
-		if (i < j && i < k) {
+		if (i < j & i < k) {
 			min = i;
 		}
 		
-		if (j < i && j < k) {
+		else if (j < i & j < k) {
 			min = j;
 		}
 		
-		if (k < i && k < j) {
+		else  {
 			min = k;
 		}
 		System.out.println("min = " + min);
 		
 		// Um das Maximum aus i, j, k zu finden, dadurch das man mehrere Vergleiche hat && benutzt
 		
-		if (i > j && i > k) {
+		if (i > j & i > k) {
 			max = i;
 		}
 		
-		if (j > i && j > k) {
+		else if (j > i & j > k) {
 			max = j;
 		}
 		
-		if (k > i && k > j) {
+		else {
 			max = k;
 		}
 		System.out.println("max = " + max);
